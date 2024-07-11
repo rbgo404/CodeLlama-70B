c9a1560186bc97fef3fe6a379bb435eb0ae75f0f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-07-11 14:26:52 +0000

    added missing files

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from vllm import LLM, SamplingParams
 
 class InferlessPythonModel:
     def initialize(self):
-
         self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95,max_tokens=256)
         self.llm = LLM(model="TheBloke/CodeLlama-70B-Python-GPTQ", quantization="gptq", dtype="float16")
 
@@ -14,4 +13,4 @@ class InferlessPythonModel:
         return {'result': result_output[0]}
 
     def finalize(self):
-        pass
+        self.llm = None
